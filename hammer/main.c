fd8e8d10160aebf0d8b6875920a7d2fd2d8f5c8a
Geoffroy Couprie <geo.couprie@gmail.com>
2015-03-03 16:11:16 +0100

    basic filetype parsing

--- hammer/main.c
+++ hammer/main.c
@@ -1,12 +1,62 @@
 #include <hammer/hammer.h>
 #include <hammer/internal.h>
+#include <hammer/glue.h>
 #include <stdio.h>
 
 struct FileType {
-  char*  major_brand;
-  char*  major_brand_version;
-  char** compatible_brands;
+  char*   major_brand;
+  char*   major_brand_version;
+  char*   compatible_brands;
 };
+
+HParsedToken *ftyp_action(const HParseResult *p, void *user_data) {
+  printf("token type: %d\n", p->ast->token_type);
+  if( p->ast->token_type == TT_SEQUENCE) {
+    char* buffer = h_arena_malloc(p->ast->seq->arena, p->ast->seq->used);
+    for (size_t i=0; i<p->ast->seq->used; ++i) {
+      buffer[i] = H_CAST_UINT(p->ast->seq->elements[i]);
+    }
+
+    HParser* brand    = h_repeat_n(h_choice(h_ch_range('0', '9'), h_ch_range('a', 'z'), NULL), 4);
+    HParser* brands   = h_many(brand);
+    HParser* ftyp     = h_sequence(brand, h_uint32(), brands, NULL);
+    HParseResult *res = h_parse(ftyp, buffer + 8, p->ast->seq->used - 8);
+
+    if(res) {
+      printf("parsed ftyp\n");
+
+      struct FileType* ft         = h_arena_malloc(p->ast->seq->arena, sizeof(struct FileType));
+      ft->major_brand             = h_arena_malloc(p->ast->seq->arena, 5);
+      ft->major_brand_version     = h_arena_malloc(p->ast->seq->arena, 5);
+      ft->compatible_brands       = h_arena_malloc(p->ast->seq->arena, (p->ast->seq->used - 8) + 1);
+
+      for (size_t i=0; i < 4; ++i) {
+        ft->major_brand[i] = H_CAST_UINT(p->ast->seq->elements[i]);
+      }
+      ft->major_brand[4] = 0;
+      for (size_t i=0; i < 4; ++i) {
+        ft->major_brand_version[i] = H_CAST_UINT(p->ast->seq->elements[i+4]);
+      }
+      ft->major_brand_version[4] = 0;
+      for (size_t i=0; i < (p->ast->seq->used - 8); ++i) {
+        ft->compatible_brands[i] = H_CAST_UINT(p->ast->seq->elements[i+8]);
+      }
+      printf("major brand: %s\n major version: %04X\ncompatible brands: %s\n",
+        ft->major_brand, ft->major_brand_version, ft->compatible_brands);
+
+      h_arena_free(p->ast->seq->arena, ft->major_brand);
+      h_arena_free(p->ast->seq->arena, ft->major_brand_version);
+      h_arena_free(p->ast->seq->arena, ft->compatible_brands);
+      h_arena_free(p->ast->seq->arena, ft);
+      h_arena_free(p->ast->seq->arena, buffer);
+      return res;
+    } else {
+      printf("did not parse ftyp\n");
+      h_arena_free(p->ast->seq->arena, buffer);
+    }
+  }
+  return NULL;
+}
 
 HParser* build_parser() {
   HParser *ftyp_tag = h_token("ftyp", 4);
@@ -23,14 +73,11 @@ HParser* build_parser() {
   HParser *trak_tag = h_token("trak", 4);
   HParser *udta_tag = h_token("udta", 4);
 
-  HParser* brand    = h_repeat_n(h_choice(h_ch_range('0', '9'), h_ch_range('a', 'z'), NULL), 4);
-  HParser* brands   = h_many(brand);
-  HParser* ftyp     = h_sequence(ftyp_tag, brand, h_uint32(), brands, NULL);
 
   HParser *tag      = h_choice(ftyp_tag, moov_tag, mdra_tag, dref_tag, cmov_tag, rmra_tag, iods_tag, mvhd_tag, clip_tag, trak_tag, udta_tag, NULL);
 
-  // does not work: the length is the offset from the beginning of the box
-  HParser *ftyp_box  = h_length_value(h_left(h_uint32(), ftyp_tag), h_uint8());
+  //HParser *ftyp_box  = h_length_value(h_left(h_uint32(), ftyp_tag), h_uint8());
+  HParser *ftyp_box  = h_action(h_length_value(h_left(h_uint32(), ftyp_tag), h_uint8()), ftyp_action, NULL);
   HParser *free_box  = h_length_value(h_left(h_uint32(), free_tag), h_uint8());
   HParser *mp4_box  = h_choice(ftyp_box, free_tag, NULL);
   //HParser *mp4_box  = h_length_value(h_left(h_uint32(), ftyp), h_uint8());
