f0b7012dad9feed2f0c60cc45f81ff55c5ff3ff1
Geoffroy Couprie <geo.couprie@gmail.com>
2015-03-03 22:24:51 +0100

    add mdat, moov and skip to attoparsec's parser

--- attoparsec/src/Main.hs
+++ attoparsec/src/Main.hs
@@ -10,7 +10,7 @@ import qualified Data.ByteString.Char8            as C8
 import           Data.Word
 
 data LV = LV Int BOX deriving Show
-data BOX = FTYP B.ByteString Word32 [B.ByteString] | FREE deriving Show
+data BOX = FTYP B.ByteString Word32 [B.ByteString] | FREE | MOOV | SKIP | MDAT deriving Show
 
 ftypBox :: C.Parser BOX
 ftypBox = do
@@ -22,15 +22,29 @@ ftypBox = do
 
 freeBox = do
   C.string "free"
-  return $ FREE
+  return  FREE
+mdat = do
+  C.string "mdat"
+  return  MDAT
+moov = do
+  C.string "moov"
+  return  MOOV
+skip = do
+  C.string "skip"
+  return SKIP
+mdra = C.string "mdra"
+dref = C.string "dref"
+cmov = C.string "cmov"
+rmra = C.string "rmra"
+
 
 mp4Box :: C.Parser LV
 mp4Box = do
   length <- fromIntegral <$> Bin.anyWord32be
-  Right value  <- C.parseOnly anyBox <$> (C.take $ length - 4)
+  Right value  <- C.parseOnly anyBox <$> C.take (length - 4)
   return $ LV length value
 
-anyBox = C.choice [ftypBox, freeBox]
+anyBox = C.choice [ftypBox, freeBox, moov, skip, mdat]
 
 mp4Parser = C.many1 mp4Box
 
