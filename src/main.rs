0bca0236d78396202c9a2da81bedd961fdb316ba
Geoffroy Couprie <geo.couprie@gmail.com>
2015-03-24 17:59:25 +0100

    Update code to nom 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ extern crate nom;
 
 extern crate test;
 
-use nom::{HexDisplay,IResult,FlatMap,FlatMapOpt,Functor,Producer,ProducerState,FileProducer,be_u16,be_u32,be_u64,be_f32,be_f64};
-use nom::{Consumer,ConsumerState};
+use nom::{HexDisplay,IResult,Needed,FlatMap,be_u32};
 use nom::IResult::*;
 
-use std::str;
-use std::io::SeekFrom;
+use std::str::from_utf8;
 
 fn mp4_box(input:&[u8]) -> IResult<&[u8], &[u8]> {
   match be_u32(input) {
@@ -18,7 +16,7 @@ fn mp4_box(input:&[u8]) -> IResult<&[u8], &[u8]> {
       if i.len() >= sz - 4 {
         return Done(&i[(sz-4)..], &i[0..(sz-4)])
       } else {
-        return Incomplete(1234)
+        return Incomplete(Needed::Size(4 + offset as u32))
       }
     }
     Error(e)      => Error(e),
@@ -44,65 +42,34 @@ enum MP4Box<'a> {
   Unknown
 }
 
-tag!(ftyp    "ftyp".as_bytes());
+named!(brand_name<&[u8],&str>, map_res!(take!(4), from_utf8));
 
-fn brand_name(input:&[u8]) -> IResult<&[u8],&str> {
-  take!(major_brand_bytes 4);
-  major_brand_bytes(input).map_res(str::from_utf8)
-}
-take!(major_brand_version 4);
-many0!(compatible_brands<&[u8], &str> brand_name);
-
-fn filetype_parser<'a>(input: &'a[u8]) -> IResult<&'a [u8], FileType<'a> > {
+fn filetype_box<'a>(input: &'a[u8]) -> IResult<&'a [u8], MP4Box > {
   //println!("ftyp:\n{}", input.to_hex(8));
   chaining_parser!(input,
-    m: brand_name          ~
-    v: major_brand_version ~
-    c: compatible_brands   ,
-    ||{FileType{major_brand: m, major_brand_version:v, compatible_brands: c}})
-}
-
-o!(filetype <&[u8], FileType>  ftyp ~ [ filetype_parser ]);
-
-fn filetype_box(input:&[u8]) -> IResult<&[u8], MP4Box> {
-  match filetype(input) {
-    Error(a)      => Error(a),
-    Incomplete(a) => Incomplete(a),
-    Done(i, o)    => {
-      Done(i, MP4Box::Ftyp(o))
-    }
-  }
-}
-
-tag!(moov_tag "moov".as_bytes());
-fn moov_box(input:&[u8]) -> IResult<&[u8], MP4Box> {
-  moov_tag(input).map(|_| MP4Box::Moov)
-}
-
-tag!(mdat    "mdat".as_bytes());
-fn mdat_box(input:&[u8]) -> IResult<&[u8], MP4Box> {
-  mdat(input).map(|_| MP4Box::Mdat)
-}
-tag!(free    "free".as_bytes());
-fn free_box(input:&[u8]) -> IResult<&[u8], MP4Box> {
-  free(input).map(|_| MP4Box::Free)
-}
-
-tag!(skip    "skip".as_bytes());
-fn skip_box(input:&[u8]) -> IResult<&[u8], MP4Box> {
-  skip(input).map(|_| MP4Box::Skip)
-}
-
-tag!(wide    "wide".as_bytes());
-fn wide_box(input:&[u8]) -> IResult<&[u8], MP4Box> {
-  wide(input).map(|_| MP4Box::Wide)
+       tag!("ftyp")       ~
+    m: brand_name         ~
+    v: take!(4)           ~
+    c: many0!(brand_name) ,
+    ||{MP4Box::Ftyp(FileType{major_brand: m, major_brand_version:v, compatible_brands: c})})
 }
 
 fn unknown_box(input:&[u8]) -> IResult<&[u8], MP4Box> {
   Done(input, MP4Box::Unknown)
 }
 
-alt!(box_parser_internal<&[u8], MP4Box>, filetype_box | moov_box | mdat_box | free_box | skip_box | wide_box | unknown_box);
+named!(box_parser_internal<&[u8], MP4Box>,
+  alt!(
+    filetype_box |
+    tag!("moov") => { |_| MP4Box::Moov } |
+    tag!("mdat") => { |_| MP4Box::Mdat } |
+    tag!("free") => { |_| MP4Box::Free } |
+    tag!("skip") => { |_| MP4Box::Skip } |
+    tag!("wide") => { |_| MP4Box::Wide } |
+    unknown_box
+  )
+);
+
 fn box_parser(input:&[u8]) -> IResult<&[u8], MP4Box> {
   mp4_box(input).flat_map(box_parser_internal)
 }
@@ -114,7 +81,7 @@ fn data_interpreter(bytes:&[u8]) -> IResult<&[u8], ()> {
     Done(i, o) => {
       /*match o {
         MP4Box::Ftyp(f) => println!("-> FTYP: {:?}", f),
-        MP4Box::Moov(m) => println!("-> MOOV: {:?}", m),
+        MP4Box::Moov    => println!("-> MOOV"),
         MP4Box::Mdat    => println!("-> MDAT"),
         MP4Box::Free    => println!("-> FREE"),
         MP4Box::Skip    => println!("-> SKIP"),
@@ -131,13 +98,13 @@ fn data_interpreter(bytes:&[u8]) -> IResult<&[u8], ()> {
       Error(a)
     },
     Incomplete(a) => {
-      println!("mp4 incomplete: {:?}", a);
+      //println!("mp4 incomplete: {:?}", a);
       Incomplete(a)
     }
   }
 }
 
-many0!(full_data_interpreter<&[u8],()> data_interpreter);
+named!(full_data_interpreter<&[u8],Vec<()> >, many0!(data_interpreter));
 
 use test::Bencher;
 #[bench]
