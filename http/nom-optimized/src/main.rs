9f1c486666c64ce22ae48e239622c58326478394
Geoffroy Couprie <geo.couprie@gmail.com>
2018-03-19 11:56:21 +0100

    use the no alloc version by default

--- http/nom-optimized/src/main.rs
+++ http/nom-optimized/src/main.rs
@@ -214,53 +214,33 @@ named!(http_version<u8>, preceded!(
     map!(one_of!("01"), |n| if n == '0' { 0 } else { 1 })
 ));
 
-named!(message_header_value, delimited!(
+named!(header_value, delimited!(
     take_while1!(is_horizontal_space),
     //take_while1!(is_header_value_token),
     take_while1_unrolled!(is_header_value_token),
     line_ending
 ));
 
-fn message_header<'a>(input: &'a [u8]) -> IResult<&'a[u8], Header<'a>> {
-  do_parse!(input,
-    name:   take_while1!(is_header_name_token) >>
-            char!(':')                         >>
-    value: message_header_value                >>
-
-    ( Header {
-        name: name,
-        value: value,
-    } )
-  )
-}
-
-fn request<'a>(input: &'a [u8]) -> IResult<&'a[u8], (Request<'a>, Vec<Header<'a>>)> {
-  terminated!(input,
-    pair!(request_line, many0!(message_header)),
-    line_ending
-  )
-}
-
-fn message_header_no_alloc<'a,'h>(input: &'a [u8], header: &'h mut Header<'a>) -> IResult<&'a[u8], ()> {
+fn header<'a,'h>(input: &'a [u8], header: &'h mut Header<'a>) -> IResult<&'a[u8], ()> {
   let (input, name) = try_parse!(input, take_while1!(is_header_name_token));
   header.name = name;
   let (input, _) = try_parse!(input, char!(':'));
-  let (input, value) = try_parse!(input, message_header_value);
+  let (input, value) = try_parse!(input, header_value);
   header.value = value;
   Ok((input, ()))
 }
 
-fn headers_no_alloc<'a,'h>(input: &'a [u8], headers: &'h mut [Header<'a>]) -> IResult<&'a[u8], ()> {
+fn headers_iter<'a,'h>(input: &'a [u8], headers: &'h mut [Header<'a>]) -> IResult<&'a[u8], ()> {
   let mut iter = headers.iter_mut();
   let mut input = input;
 
   loop {
-    let header = match iter.next() {
+    let h = match iter.next() {
       Some(header) => header,
       None => break
     };
 
-    match message_header_no_alloc(input, header) {
+    match header(input, h) {
       Ok((i, _)) => input = i,
       Err(nom::Err::Error(_)) => break,
       e => return e,
@@ -270,11 +250,11 @@ fn headers_no_alloc<'a,'h>(input: &'a [u8], headers: &'h mut [Header<'a>]) -> IR
   Ok((input, ()))
 }
 
-fn request_no_alloc<'a,'h>(input: &'a [u8], headers: &'h mut [Header<'a>]) -> IResult<&'a[u8], Request<'a>> {
+fn request<'a,'h>(input: &'a [u8], headers: &'h mut [Header<'a>]) -> IResult<&'a[u8], Request<'a>> {
   terminated!(input,
     request_line,
     pair!(
-      apply!(headers_no_alloc, headers),
+      apply!(headers_iter, headers),
       line_ending
     )
   )
@@ -323,48 +303,6 @@ Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; __utma=xxxxxx
   parse(b, data)
 }
 
-fn small_no_alloc_test(b: &mut Bencher) {
-  let data = include_bytes!("../../http-requests.txt");
-  b.bytes = data.len() as u64;
-  parse_no_alloc(b, data)
-}
-
-fn bigger_no_alloc_test(b: &mut Bencher) {
-  let data = include_bytes!("../../bigger.txt");
-  b.bytes = data.len() as u64;
-  parse_no_alloc(b, data)
-}
-
-fn one_no_alloc_test(b: &mut Bencher) {
-  let data = &b"GET / HTTP/1.1
-Host: www.reddit.com
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:15.0) Gecko/20100101 Firefox/15.0.1
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-Accept-Language: en-us,en;q=0.5
-Accept-Encoding: gzip, deflate
-Connection: keep-alive
-
-"[..];
-  b.bytes = data.len() as u64;
-  parse_no_alloc(b, data)
-}
-
-fn httparse_example_no_alloc_test(b: &mut Bencher) {
-  let data = &b"GET /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg HTTP/1.1\r\n\
-Host: www.kittyhell.com\r\n\
-User-Agent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; ja-JP-mac; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 Pathtraq/0.9\r\n\
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n\
-Accept-Language: ja,en-us;q=0.7,en;q=0.3\r\n\
-Accept-Encoding: gzip,deflate\r\n\
-Accept-Charset: Shift_JIS,utf-8;q=0.7,*;q=0.7\r\n\
-Keep-Alive: 115\r\n\
-Connection: keep-alive\r\n\
-Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n\r\n"[..];
-
-  b.bytes = data.len() as u64;
-  parse_no_alloc(b, data)
-}
-
 #[test]
 fn httparse_test() {
   let data = &b"GET /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg HTTP/1.1\r\n\
@@ -378,58 +316,20 @@ Keep-Alive: 115\r\n\
 Connection: keep-alive\r\n\
 Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n\r\n"[..];
 
-  let res = request(data);
-  println!("res:\n{:?}", res);
-  res.unwrap();
-}
-
-#[test]
-fn httparse_no_alloc_test() {
-  let data = &b"GET /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg HTTP/1.1\r\n\
-Host: www.kittyhell.com\r\n\
-User-Agent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; ja-JP-mac; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 Pathtraq/0.9\r\n\
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n\
-Accept-Language: ja,en-us;q=0.7,en;q=0.3\r\n\
-Accept-Encoding: gzip,deflate\r\n\
-Accept-Charset: Shift_JIS,utf-8;q=0.7,*;q=0.7\r\n\
-Keep-Alive: 115\r\n\
-Connection: keep-alive\r\n\
-Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n\r\n"[..];
-
   let mut headers = [Header{ name: &[], value: &[] }; 16];
-  let res = request_no_alloc(data, &mut headers);
+  let res = request(data, &mut headers);
   println!("res:\n{:?}\nheaders:\n{:?}", res, headers);
   res.unwrap();
 }
 
 fn parse(b: &mut Bencher, buffer: &[u8]) {
-    b.iter(|| {
-        let mut buf = black_box(buffer);
-        let mut v = Vec::new();
-
-        while !buf.is_empty() {
-            match request(buf) {
-                Ok((i, o)) => {
-                    v.push(o);
-
-                    buf = i
-                }
-                Err(err) => panic!("got err: {:?}", err),
-            }
-        }
-
-        v
-    });
-}
-
-fn parse_no_alloc(b: &mut Bencher, buffer: &[u8]) {
     let mut headers = [Header{ name: &[], value: &[] }; 16];
     b.iter(|| {
         let mut buf = black_box(buffer);
         let mut v = Vec::new();
 
         while !buf.is_empty() {
-            match request_no_alloc(buf, &mut headers) {
+            match request(buf, &mut headers) {
                 Ok((i, o)) => {
                     v.push(o);
 
@@ -444,8 +344,7 @@ fn parse_no_alloc(b: &mut Bencher, buffer: &[u8]) {
 }
 
 benchmark_group!(http, one_test, small_test, bigger_test, httparse_example_test);
-benchmark_group!(http_no_alloc, one_no_alloc_test, small_no_alloc_test, bigger_no_alloc_test, httparse_example_no_alloc_test);
-benchmark_main!(http, http_no_alloc);
+benchmark_main!(http);
 
 /*
 fn main() {
