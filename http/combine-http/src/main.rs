9c01e7b2f6a13468ea41d43ed19a82b412f4296c
Geoffroy Couprie <geo.couprie@gmail.com>
2018-03-23 16:57:42 +0100

    add a benchmark to combine-http

--- http/combine-http/src/main.rs
+++ http/combine-http/src/main.rs
@@ -132,6 +132,22 @@ Connection: keep-alive
   parse(b, data)
 }
 
+fn httparse_example_test(b: &mut Bencher) {
+  let data = &b"GET /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg HTTP/1.1\r\n\
+Host: www.kittyhell.com\r\n\
+User-Agent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; ja-JP-mac; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 Pathtraq/0.9\r\n\
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n\
+Accept-Language: ja,en-us;q=0.7,en;q=0.3\r\n\
+Accept-Encoding: gzip,deflate\r\n\
+Accept-Charset: Shift_JIS,utf-8;q=0.7,*;q=0.7\r\n\
+Keep-Alive: 115\r\n\
+Connection: keep-alive\r\n\
+Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n\r\n"[..];
+
+  b.bytes = data.len() as u64;
+  parse(b, data)
+}
+
 fn parse(b: &mut Bencher, buffer: &[u8]) {
     b.iter(|| {
         let mut buf = black_box(buffer);
@@ -153,6 +169,6 @@ fn parse(b: &mut Bencher, buffer: &[u8]) {
     });
 }
 
-benchmark_group!(http, one_test, small_test, bigger_test);
+benchmark_group!(http, one_test, small_test, bigger_test, httparse_example_test);
 benchmark_main!(http);
 
